6ddbc7d5aec9649cff6014d29361c5f3186c543e
Anfo270 <andres44chuy@gmail.com>
2022-10-17 12:14:25 -0600

    login 2

--- conexionbd.php
+++ conexionbd.php
@@ -1,4 +1,4 @@
-<?
+<?php
     session_start();
     //datos de db
     $server='localhost';
--- login.php
+++ login.php
@@ -1,5 +1,14 @@
-<?
+<?php
 include('conexionbd.php');
+$server='localhost';
+$user='root';
+$pass='';
+$db='control_inventario';
+
+//conexion a base de datos
+$conexion=new mysqli($server,$user,$pass,$db);
+//verificacion de error
+    
 
 $user=$_POST['user'];
 $pass=$_POST['pass'];
@@ -7,12 +16,16 @@ $pass=$_POST['pass'];
 //buscar usuario
 $query=mysqli_query($conexion,"SELECT * FROM usuarios WHERE Usuario='".$user."'and Contraseña='".$pass."'");
 //asignamos el valor en una variable, si es 0 no existe
-$uss=$query;
+$uss=mysqli_num_rows($query);
 
 if($uss!=0){
     $_SESSION['user']=$user;
     $_SESSION['pass']=$pass;
-    header("Location: menu.html");
+ 
+    header('location: menu.html');
+
+}else{
+    header('location: index.html');
 }
 
 ?>
