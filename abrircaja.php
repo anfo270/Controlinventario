ed0e21a270774572708fba79647e7d32f1b8d340
Cat271 <99299404+Cat271@users.noreply.github.com>
2022-10-17 13:21:12 -0600

    Salto de linea en la 21

--- abrircaja.php
+++ abrircaja.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION['Usuario'])&& !isset( $_SESSION['Contraseña'])){
 <body>
     <nav><button class="btn cerrar" onclick="location.href='cerrar.php'">Cerrar Sesion</button> <p>Jesus Flores</p></nav>
     <div class="contenedor">
-        <button class="btn abrircaja" onclick="location.href='seccionventas.html'">Abrir Caja</button>
+        <button class="btn abrircaja" onclick="location.href='seccionventas.html'">Abrir Caja</button><br>
         <h3>Fecha:</h3><p id="fecha">0/0/0</p>
         <h3>Hora</h3><p id="hora">00:00</p>
     </div> 
