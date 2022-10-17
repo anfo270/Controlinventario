dd1874eaf6dfeaf0e2d6c739760821b6294447bd
Anfo270 <andres44chuy@gmail.com>
2022-10-17 11:08:46 -0600

    login

--- conexionbd.php
+++ conexionbd.php
@@ -1,14 +1,15 @@
 <?
-$servername = "localhost";
-$database = "control_inventario";
-$username = "root";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn)
+    $servername = "localhost";
+    $database = "control_inventario";
+    $username = "root";
+    $password = "";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+    mysqli_close($conn)
+    
 ?>
