681f5929ea2cb1971ca73e4c52089a2fa119e041
Anfo270 <andres44chuy@gmail.com>
2022-10-23 16:57:39 -0600

    agregar comentarios

--- Config/eliminar.php
+++ Config/eliminar.php
@@ -1,8 +1,9 @@
 <?php
     session_start();
     include('conexionbd.php');
+    //variable de ID 
     $id=intval($_GET['ID_usuarios']);
-
+    //borramos el registro
     $res=$conexion->query("DELETE FROM usuarios WHERE ID='$id'") or die(print($conexion->errorInfo()));
     if($res){
         echo("<script>Se elimino existosamente el usuario</script>");
