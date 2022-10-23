bd45a838a3233c93b3b443ba325fb89c66c88a43
Anfo270 <andres44chuy@gmail.com>
2022-10-23 11:04:56 -0600

    correcion de conexion de diseño

--- menu.php
+++ menu.php
@@ -27,8 +27,8 @@ $usu = $_SESSION['Usuario']
     <div class="contenedor">
         <button class="btn ventas" onclick="location.href='menu_ventas/abrircaja.php'">Vendedor</button>
         <button class="btn inventario" onclick="location.href='menu_responsable/inventario.php'">Responsable</button>
-        <button class="btn administrador" onclick="location.href='administrador.php'">Administrador</button>
-        <button class="btn administrador" onclick="location.href='sistema.php'">Sistema</button>
+        <button class="btn administrador" onclick="location.href='menu_administrador/administrador.php'">Administrador</button>
+        <button class="btn administrador" onclick="location.href='menu_sistema/sistema.php'">Sistema</button>
     </div>
 </body>
 </html>
