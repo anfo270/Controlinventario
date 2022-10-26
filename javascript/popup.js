eba4989bb02986b798d537c06c295fe71e5a362c
Anfo270 <andres44chuy@gmail.com>
2022-10-26 10:39:11 -0600

    agregar usuarios

--- javascript/popup.js
+++ javascript/popup.js
@@ -2,6 +2,6 @@ const open =document.getElementById('eliminar');
 const model =document.getElementById('modal-contenedor');
 const aceptar =document.getElementById('aceptar');
 
-document.getElementById('eliminar').addEventListener('click',  ()  => {
+document.getElementById('eliminar').addEventListener('click',  (event) => {
     document.getElementById('modal-contenedor').classList.add('show');    
 });
