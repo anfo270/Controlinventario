f5effde465e9772dc9b35742bbf91634b63d8e92
Cat271 <cristianalexis271@gmail.com>
2022-11-02 23:07:31 -0600

    Modificacion temporal en boton reset

--- Config/borrartablas.php
+++ Config/borrartablas.php
@@ -3,5 +3,8 @@ session_start();
 if($_POST['pass']!=$_SESSION['Contraseña']){
     echo '<script>alert("Contraseña incorrecta")</script>';
     echo ("<script>location.href='../menu_sistema/sistema.php'</script>");
+}else{
+    echo '<script>alert("Esta funci\u00F3n no est\u00e1 disponible por el momento.")</script>';
+    echo ("<script>location.href='../menu_sistema/sistema.php'</script>");
 }
 ?>
