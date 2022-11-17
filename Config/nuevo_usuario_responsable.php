e808914d5ffc68bbfc824a4c2c9664cac81dfdf6
Anfo270 <andres44chuy@gmail.com>
2022-11-17 12:43:48 -0600

    edicion de locacion para responsable

--- Config/nuevo_usuario_responsable.php
+++ Config/nuevo_usuario_responsable.php
@@ -1,19 +1,14 @@
 <?php
 include('conexionbd.php');
-
-$Nombre=$_POST['Nombre'];
-$Apellido_Paterno=$_POST['Apellido_Paterno'];
-$Apellido_Materno=$_POST['Apellido_Materno'];
-$Contraseña=$_POST['Contraseña'];
-$puesto=$_POST['Puesto'];
+session_start();
 $Local=$_POST['Local'];
-$Usuario=$Nombre." ".$Apellido_Paterno;
 
 if($_GET['identificador']==2){
     $id_use=$_GET['id'];
     //revisar
-    $actualizar=actualizar($Nombre,$Apellido_Paterno,$Apellido_Materno,$Contraseña,$puesto,$Local,$Usuario,$conexion,$id_use);
+    $actualizar=actualizar($conexion,$id_use,$Local);
     if($actualizar){
+        $_SESSION['Local']=$Local;
         echo "<script>alert('Se modifico el Usuario');</script>"; 
         echo "<script>location.href='../menu_responsable/usuarios.php'</script>";
     }else{
@@ -26,18 +21,9 @@ if($_GET['identificador']==2){
 }
 
 
-function actualizar($Nombre,$Apellidos_Paterno,$Apellido_Materno,$Contraseña,$puesto,$Local,$Usuario,$conexion,$id_use){
-    $res="UPDATE usuarios SET Nombre='$Nombre' , Apellido_Paterno='$Apellidos_Paterno', Apellido_Materno='$Apellido_Materno' ,  Usuario='$Usuario',
-                            Contraseña='$Contraseña' ,  Puesto='$puesto' ,  Local='$Local' WHERE ID='$id_use'" ;
-    $res=$conexion->query($res) or die(print($conexion->errorInfo()));
+function actualizar($conexion,$id_use,$local){
+    $Local="Local";
+    $res=$conexion->query("UPDATE usuarios SET $Local='$local' WHERE ID='$id_use'") or die(print($conexion->errorInfo()));
     return $res;
 }
-
-function insert($Nombre,$Apellido_Paterno,$Apellido_Materno,$Contraseña,$puesto,$Local,$Usuario,$conexion){
-    $res="INSERT INTO usuarios  (  Nombre ,  Apellido_Paterno, Apellido_Materno ,  Usuario ,  Contraseña ,  Puesto ,  Local ) 
-    values ( '$Nombre' , '$Apellido_Paterno','$Apellido_Materno' , '$Usuario' , '$Contraseña' , '$puesto' , '$Local' )";
-    $res=$conexion->query($res) or die(print($conexion->errorInfo()));
-    return $res;
-}
-
 ?>
