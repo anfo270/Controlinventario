4f6bde0ead9a9e33b9be10bc5aa5b77cf38111dc
Cat271 <cristianalexis271@gmail.com>
2022-11-23 21:15:34 -0600

    Conexion alterna

--- Config/conexionbd.php
+++ Config/conexionbd.php
@@ -1,5 +1,11 @@
 <?php
     //datos de db
+
+    /*$server='localhost';
+    $user='movil_sistemas';
+    $pass='Sistema.pv2022';
+    $db='movil_sistemas';*/
+
     $server='localhost';
     $user='root';
     $pass='';
