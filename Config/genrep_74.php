fde7459ff5a3af0c2f9850e82706b93f45f0af6d
Cat271 <cristianalexis271@gmail.com>
2022-12-19 23:01:30 -0700

    Actualizacion de formato en Reporte y suma de total

--- Config/genrep_74.php
+++ Config/genrep_74.php
@@ -43,7 +43,9 @@ $writer->setAuthor($creadorArchivo);
 
 // Estilos
 $styles1 = array( 'font'=>'Calibri','font-size'=>11,'font-style'=>'bold', 'fill'=>'#eee', 'halign'=>'center', 'border'=>'left,right,top,bottom' );
-$styles2 = array( 'font'=>'Calibri','font-size'=>11,'halign'=>'center', 'border'=>'left,right,top,bottom' );
+$styles2 = array( 'font'=>'Calibri','font-size'=>11,'halign'=>'left', 'border'=>'left,right,top,bottom' );
+$styles3 = array( array(),array(),array(),array(),array(),array(),array(),$styles1,$styles1,array());
+$styles4 = array( array(),array(),array(),array(),array(),array(),array(),$styles2,$styles2,array());
 $header = array("string","string","string","string");
 
 //$senal=intval(1);
@@ -56,28 +58,28 @@ if ($senal==1){
     $consulta_sims=$conexion->query("SELECT * FROM sims WHERE Locacion='$local' ")or die(print_r($conexion->errorInfo()));
 
     // Especificar header
-    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,15,20,25], 'suppress_row'=>true] );
-    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Tipo','Marca','Modelo','Locacion'), $styles1);
+    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,15,20,25,15], 'suppress_row'=>true] );
+    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Tipo','Marca','Modelo','Locacion','Fecha Ingreso'), $styles1);
 
     // Comenzar rellenado de datos
     // Todos los telefonos
     while ($item=$consulta_telefonos->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->IMEI,'Equipo',$item->Marca,$item->Modelo,$item->Locacion
+            $item->IMEI,'Equipo',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaTraspaso
         ), $styles2);
     }
 
     // Todos los accesorios
     while ($item=$consulta_accesorios->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->SKU,'Accesorio',$item->Marca,$item->Modelo,$item->Locacion
+            $item->SKU,'Accesorio',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaTraspaso
         ), $styles2);
     }
     
     // Todos los SIMs
     while ($item=$consulta_sims->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->ICC,'SIM card',$item->Marca,$item->Modelo,$item->Locacion
+            $item->ICC,'SIM card',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaTraspaso
         ), $styles2);
     }
 
@@ -94,28 +96,28 @@ if ($senal==1){
     $consulta_sims=consulta($conexion, "sims");
 
     // Especificar header
-    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,15,20,25], 'suppress_row'=>true] );
-    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Tipo','Marca','Modelo','Locacion'), $styles1);
+    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,15,21,27,15,15,25], 'suppress_row'=>true] );
+    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Tipo','Marca','Modelo','Locacion','Fecha Ingreso','Costo','Proveedor'), $styles1);
 
     // Comenzar rellenado de datos
     // Todos los telefonos
     while ($item=$consulta_telefonos->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->IMEI,'Equipo',$item->Marca,$item->Modelo,$item->Locacion
+            $item->IMEI,'Equipo',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaIngreso,$item->Precio,$item->Proveedor
         ), $styles2);
     }
 
     // Todos los accesorios
     while ($item=$consulta_accesorios->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->SKU,'Accesorio',$item->Marca,$item->Modelo,$item->Locacion
+            $item->SKU,'Accesorio',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaIngreso,$item->Precio,$item->Proveedor
         ), $styles2);
     }
     
     // Todos los SIMs
     while ($item=$consulta_sims->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->ICC,'SIM card',$item->Marca,$item->Modelo,$item->Locacion
+            $item->ICC,'SIM card',$item->Marca,$item->Modelo,$item->Locacion,$item->FechaIngreso,$item->Precio,$item->Proveedor
         ), $styles2);
     }
 
@@ -144,25 +146,36 @@ if ($senal==1){
     // o si se hara una consulta general entre un lapzo de tiempo
     if($cortecaja=='cortecaja'){
         $consulta_ventas=$conexion->query("SELECT * FROM ventas WHERE Locacion='$local' AND Fecha BETWEEN '$data' AND '$data2'");
+        $consulta_suma_ventas=$conexion->query("SELECT SUM(Precio) AS TotalVentas FROM ventas WHERE Locacion='$local' AND Fecha BETWEEN '$data' AND '$data2'");
     }else{ 
         $consulta_ventas=$conexion->query("SELECT * FROM ventas WHERE Fecha BETWEEN '$data' AND '$data2'");
+        $consulta_suma_ventas=$conexion->query("SELECT SUM(Precio) AS TotalVentas FROM ventas WHERE Fecha BETWEEN '$data' AND '$data2'");
+    }
+
+    if($consulta_suma_ventas->rowCount() == 1){
+        $suma_ventas = $consulta_suma_ventas->fetch();
+        $totalVentas = $suma_ventas['TotalVentas'];
     }
 
     // Especificar header
-    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,20,15,15,15,15,15,15], 'suppress_row'=>true] );
-    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Marca','Modelo','Vendedor','Perfil vendedor', 'Fecha', 'Locacion','Precio', 'Financiera'), $styles1);
+    $writer->writeSheetHeader('Hoja1', $header, $col_options = ['widths'=>[25,15,20,23,15,15,20,15,15,30], 'suppress_row'=>true] );
+    $writer->writeSheetRow('Hoja1', $rowdata = array('IMEI/ICC/SKU','Marca','Modelo','Vendedor','Perfil vendedor', 'Fecha', 'Locacion','Precio', 'Financiera','Comentarios'), $styles1);
 
     // Comenzar rellenado de datos
     // Ventas realizadas
     while ($item=$consulta_ventas->fetch(PDO::FETCH_OBJ)){
         $writer->writeSheetRow('Hoja1', $rowdata = array(
-            $item->IMEIICCSKU,$item->Marca,$item->Modelo,$item->Vendedor,$item->TipoVendedor,$item->Fecha,$item->Locacion,$item->Precio,$item->Financiera
+            $item->IMEIICCSKU,$item->Marca,$item->Modelo,$item->Vendedor,$item->TipoVendedor,$item->Fecha,$item->Locacion,$item->Precio,$item->Financiera,$item->Comentarios
         ), $styles2);
     }
+    $writer->writeSheetRow('Hoja1',$rowdata = array(''));
+    $writer->writeSheetRow('Hoja1',$rowdata = array(''));
+    $writer->writeSheetRow('Hoja1',$rowdata = array('','','','','','','','','Total ventas',''),$styles3);
+    $writer->writeSheetRow('Hoja1',$rowdata = array('','','','','','','','',$totalVentas,''),$styles4);
 
     // Generamos el archivo
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header('Content-Disposition: attachment;filename="ReporteCobranza_' . $data . '.xlsx"');
+    header('Content-Disposition: attachment;filename="ReporteCobranza_del_' . $data . '_al_' . $data2 . '.xlsx"');
     header('Cache-Control: max-age=0');
     $writer->writeToStdOut();
 
