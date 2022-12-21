943766df8e6bbb101170bf33cb7d09b59f4ed8e4
Cat271 <cristianalexis271@gmail.com>
2022-12-21 14:17:30 -0700

    Cambio de ruta

--- Config/cancelartraspaso.php
+++ Config/cancelartraspaso.php
@@ -6,16 +6,16 @@
     //variable de ID 
     if($_POST['pass']!=$_SESSION['Contrasena']){
         echo("<script>alert('Contraseña incorrecta')</script>");
-        echo ("<script>location.href='../menu_adminstrador/vertraspasos.php'</script>");
+        echo ("<script>location.href='../menu_administrador/vertraspasos.php'</script>");
     }else{
         //borramos el registro
         $res=$conexion->query("UPDATE traspaso SET Estado='CANCELADO' WHERE IMEIICC='$id' AND NumTraspaso='$tras'");
         if($res){
             echo("<script>alert('Se cancelo exitosamente el traspaso')</script>");
-            echo ("<script>location.href='../menu_adminstrador/vertraspasos.php'</script>");
+            echo ("<script>location.href='../menu_administrador/vertraspasos.php'</script>");
         }else{
             echo("<script>alert('No se pudo cancelar')</script>");
-            echo ("<script>location.href='../menu_adminstrador/vertraspasos.php'</script>");
+            echo ("<script>location.href='../menu_administrador/vertraspasos.php'</script>");
         }
     }
 ?>
